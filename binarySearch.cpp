e1387f7b127d8f5d486776448bd1e406723b36e8
Mzahdul <seeglas8@gmail.com>
2026-04-14 10:48:10 +0700

    ke 4

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -5,11 +5,11 @@ int element[10];
 int nPanjang;
 int x;
 
-void input() 
+void input()
 {
     while (true)
     {
-        cout << "masukan banyaknya element pada array (maksimal 10): ";
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
         cin >> nPanjang;
 
         if (nPanjang <= 10)
@@ -18,17 +18,36 @@ void input()
         }
         else
         {
-            cout << "\n[!] jumlah element tidak boleh lebih dari 10. silakan coba lagi.\n" << endl;
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
         }
     }
 
-    cout << "\n===================================\n";
-    cout << "      masukan element pada array   : \n";
-    cout << "===================================\n";
+    cout << "\n==============================\n";
+    cout << "      Masukkan Elemen Array    \n";
+    cout << "==============================\n";
 
     for (int i = 0; i < nPanjang; i++)
     {
-        cout << " element ke-" << (i + 1) << " : ";
+        cout << "Data ke-" << (i + 1) << " = ";
         cin >> element[i];
     }
 }
+
+void bublesortarray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang -1);
+}
+
