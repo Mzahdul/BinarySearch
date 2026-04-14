14d9354fe85eab920d5f71c1eac6cec67e151659
Mzahdul <seeglas8@gmail.com>
2026-04-14 10:50:29 +0700

    ke 6

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -53,6 +53,18 @@ void bublesortarray()
 
 void display()
 {
+    cout << "\n==============================================\n";
+    cout << "  Elemen Array Setelah Diurutkan (Asc)\n";
+    cout << "==============================================\n";
 
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
 }
 
