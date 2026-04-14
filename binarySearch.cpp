ca77d32ad083edd55270e12a3622e656fda5318f
Mzahdul <seeglas8@gmail.com>
2026-04-14 10:36:17 +0700

    ke 2

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -4,3 +4,22 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input() 
+{
+    while (true)
+    {
+        cout << "masukan banyaknya element pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] jumlah element tidak boleh lebih dari 10. silakan coba lagi.\n" << endl;
+        }
+    }
+
+}
