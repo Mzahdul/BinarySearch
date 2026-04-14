30cccda79938e284251fd354c50d36f93fb369b4
Mzahdul <seeglas8@gmail.com>
2026-04-14 11:03:41 +0700

    ke 10

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -83,6 +83,44 @@ void binarySearch()
         int low = 0;
         int high = nPanjang - 1;
 
+        do
+        {
+            int mid = (low + high) / 2;
+
+            if (element[mid] == x)
+            {
+                cout << "\n[V] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        
+        } while (low <= high);
+
+        if (low > high)
+        {
+            cout << "\n[X] Elemen " << x << " tidak ditemukan dalam array.\n";
+        }
+
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
 
     } while (ulang == 'y' || ulang == 'Y');
+}
+
+int main ()
+{
+    input();
+    bublesortarray();
+    display();
+    binarySearch();
+
 }
